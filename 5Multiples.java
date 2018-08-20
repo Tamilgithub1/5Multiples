9acc9199b45318df12c19a2e72cf8d1e2e3f1f83
Tamilgithub1 <31219547+Tamilgithub1@users.noreply.github.com>
2018-08-20 22:17:49 +0530

    Create 5Multiples.java

---
+++ 5Multiples.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class wifi{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		for(int i=n;i<=n*5;i+=n){
+			System.out.print(i+" ");
+		}
+	}
+}
